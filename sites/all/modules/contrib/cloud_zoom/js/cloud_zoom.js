ba84f5dfa8febe2777d18b32253989fccc4c7277
Ion <macaria.ion@gmail.com>
2017-11-10 16:24:18 +0200

    last updates

--- sites/all/modules/contrib/cloud_zoom/js/cloud_zoom.js
+++ sites/all/modules/contrib/cloud_zoom/js/cloud_zoom.js
@@ -2,24 +2,9 @@
 Drupal.behaviors.cloudZoom = {
   attach: function (context, settings) {
     items = $('.cloud-zoom:not(cloud-zoom-processed), .cloud-zoom-gallery:not(cloud-zoom-processed)', context);
-    
     if (items.length) {
       items.addClass('cloud-zoom-processed').CloudZoom();
       items.parent().css('float', 'left');
-
-      // Colorbox integration
-      items.each(function() {
-        if ($(this).hasClass('colorbox')) {
-          // Add click event to wrapper (else mouse trap blocks it)
-          // But only if colorbox was initialized for this field.
-          var $item = $(this);
-          $item.parent().click(function() {
-            var bla = $('.cloud-zoom-gallery-thumbs a[href="' + $item.attr('href') +'"]');
-            bla.colorbox(jQuery.extend({open: true}, settings.colorbox));
-            return false;
-          });
-        }
-      });
     }
   }
 };
